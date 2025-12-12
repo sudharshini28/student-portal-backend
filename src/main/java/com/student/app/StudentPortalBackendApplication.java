6cddbb715b5226aa05d24b3eb81e6248769942f8
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-12-12 13:58:19 +0530

    Disabled DataSource auto config for render deployment

--- src/main/java/com/student/app/StudentPortalBackendApplication.java
+++ src/main/java/com/student/app/StudentPortalBackendApplication.java
@@ -2,12 +2,12 @@ package com.student.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StudentPortalBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentPortalBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPortalBackendApplication.class, args);
+    }
 }
